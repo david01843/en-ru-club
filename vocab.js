64baa91b097703989efcc429ba4472e412c098fa
David Zeugner <67552061+david01843@users.noreply.github.com>
2025-10-11 16:50:11 +0300

    Add files via upload

--- vocab.js
+++ vocab.js
@@ -1,5 +1,13 @@
-const toggle = document.querySelector("#vocab button")
-toggle.addEventListener("click", (e) => {
+chars = ["ё1234567890-=йцукенгшщзхъ\\фывапролджэячсмитьбю,", "`1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./"]
+codes = [192, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 189, 187, 81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 219, 221, 220, 65, 83, 68, 70, 71, 72, 74, 75, 76, 186, 222, 90, 88, 67, 86, 66, 78, 77, 188, 190, 191]
+shift = false
+lang = 0
+active = null
+audio = new SpeechSynthesisUtterance()
+audio.lang = "ru"
+
+const language = document.querySelector("#vocab #language")
+language.addEventListener("click", (e) => {
 	lang = (lang+1)%2
 	labelkeys()
 	active = null
@@ -12,8 +20,22 @@ toggle.addEventListener("click", (e) => {
 const keys = document.querySelectorAll("#vocab .row p")
 const prompt = document.querySelector("#vocab #prompt")
 const answer = document.querySelector("#vocab #answer")
+const lists = document.querySelector("#vocab #lists")
 const controls = document.querySelectorAll("#vocab #controls p")
-readxlsx = document.querySelector("#vocab input")
+controls[0].addEventListener("click", () => {readxlsx.click()})
+controls[1].addEventListener("click", () => {
+	var d = []
+	document.querySelectorAll("#vocab #words p").forEach(i => d.push([i.dataset.ans, i.innerHTML]))
+	const b = XLSX.utils.book_new()
+	const s = XLSX.utils.aoa_to_sheet(d)
+	XLSX.utils.book_append_sheet(b, s, "Sheet1")
+	XLSX.writeFile(b, "words_download.xlsx")
+})
+controls[2].addEventListener("click", () => {
+	Array.from(document.querySelectorAll("#words p")).forEach(i => i.remove())
+	cycle()
+})
+const readxlsx = document.querySelector("#vocab input")
 readxlsx.onchange = (e1) => {
 	const f = e1.target.files[0]
 	if (!f) return
@@ -27,50 +49,24 @@ readxlsx.onchange = (e1) => {
 	}
 	r.readAsArrayBuffer(f)
 }
-controls[0].addEventListener("click", () => {readxlsx.click()})
-controls[1].addEventListener("click", () => {
-	let a = Array.from(document.querySelectorAll("#words p"))
-	for (let i = a.length-1; i > 0; i--) {
-		let j = Math.floor(Math.random()*(i+1))
-		let k = a[i]; a[i] = a[j]; a[j] = k
-	}
-	a.slice(10).forEach(i => i.remove())
-	cycle()
-})
-controls[2].addEventListener("click", () => {
-	Array.from(document.querySelectorAll("#words p")).forEach(i => i.remove())
-	cycle()
-})
-controls[3].addEventListener("click", () => {
-	for (let i = 0; i < exdata[0].length; i++) {addword(exdata[lang][i], exdata[(lang+1)%2][i])}
-	cycle()
-})
-controls[4].addEventListener("click", () => {
-	var d = []
-	document.querySelectorAll("#vocab #words p").forEach(i => d.push([i.dataset.ans, i.innerHTML]))
-	const b = XLSX.utils.book_new()
-	const s = XLSX.utils.aoa_to_sheet(d)
-	XLSX.utils.book_append_sheet(b, s, "Sheet1")
-	XLSX.writeFile(b, "words_download.xlsx")
-})
 words = document.querySelector("#vocab #words")
-exdata = [["ноль", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять", "десять"], ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]]
-chars = ["ё1234567890-=йцукенгшщзхъ\\фывапролджэячсмитьбю,", "`1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./"]
-codes = [192, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 189, 187, 81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 219, 221, 220, 65, 83, 68, 70, 71, 72, 74, 75, 76, 186, 222, 90, 88, 67, 86, 66, 78, 77, 188, 190, 191]
-shift = false
-lang = 0
-active = null
-audio = new SpeechSynthesisUtterance()
-audio.lang = "ru"
-labelkeys()
-clear.click()
 
+data.forEach(i => {
+	var p = document.createElement("p")
+	p.innerHTML = i[0]
+	p.addEventListener("click", () => {
+		for (j = 1; j < i.length; j += 2) {addword(i[j], i[j+1])}
+		cycle()
+	})
+	lists.appendChild(p)
+})
 function labelkeys() {for (let i = 0; i < 47; i++) {keys[i].innerHTML = chars[lang][i]}}
+labelkeys()
 function addword(s1, s2) {
 	w = document.createElement("p")
 	w.dataset.ans = s1 // answer
 	w.innerHTML = s2 // prompt
-	w.addEventListener("click", () => {w.remove()})
+	w.addEventListener("click", (e) => {e.target.remove()})
 	words.appendChild(w)
 }
 function check() {
